75efa3cd5050e85614158ef8811112da3d727e43
Iván Mota <pinfake@hotmail.com>
2014-04-07 22:31:02 +0200

    Forgot .PHONY!

---
+++ test/all.js
@@ -0,0 +1,7 @@
+var should = require('chai').should();
+
+describe('Config parsing', function() {
+    it('parseConfig will throw exception on malformed JSON config file', function() { 
+        'hola'.should.equal( 3 ); 
+    });
+});
