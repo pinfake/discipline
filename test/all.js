ecf080e48da290e1e56244cc616623ddf96ecb38
Iván Mota <pinfake@hotmail.com>
2014-04-08 20:56:16 +0200

    Started actual coding the bdd way, two tests written and passing.

--- test/all.js
+++ test/all.js
@@ -1,7 +1,18 @@
 var should = require('chai').should();
+var Config = require('../lib/config.js');
 
 describe('Config parsing', function() {
-    it('parseConfig should throw exception on malformed JSON config file', function() { 
-        'hellew'.should.equal( 'hellew' ); 
+    
+    var config = new Config();
+
+    it('parseConfig should throw SyntaxError on malformed JSON config file', function() { 
+        var jsonString = 'wowowow';
+        (function() {config.parseConfig( jsonString )}).should.throw( SyntaxError ); 
+    });
+    it('parseConfig should throw ParseError on missing global property', function() {
+        var jsonString = '{ "test": "test" }';
+        (function() {config.parseConfig( jsonString )}).should.throw( SyntaxError,
+            '"global" property is not defined'
+         ); 
     });
 });
